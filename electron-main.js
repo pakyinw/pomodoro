fd7589e7c29a43d0ae2ea41d02a4dff7e2cb5fc8
Leo Wong <pakyinw@hotmail.com>
2023-08-16 16:47:27 +0100

    chore: install electron

---
+++ electron-main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true // Allow Node.js integration in your Vue components
+    }
+  });
+
+  mainWindow.loadFile('dist/index.html'); // Load your Vue app's HTML file
+});
